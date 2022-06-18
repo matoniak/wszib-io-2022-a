89e21985b6ac5d01a83e7b4a4f3cb40d769e9b10
Marcin Józef Matoniak <matoniak@artemis.wszib.edu.pl>
2022-06-18 16:21:58 +0200

    Optymalizacja: uwspólnienie kodu

--- trojkaty.java
+++ trojkaty.java
@@ -4,24 +4,49 @@
 
 class Trojkaty {
     /**
+                * Funkcja zwraca ile par odcinków trójkąta ma jednakową długość.
+                * @param {float} a - Długość pierwszego boku.
+                * @param {float} b - Długość drugiego boku.
+                * @param {float} c - Długość trzeciego boku.
+                */
+               public static int ileJednakowych(float a, float b, float c){
+                   int jednakowe = 0; // tutaj zliczamy ile jest jednakowych bokow
+                   if (a == b) {
+                      jednakowe++;
+                   }
+                   if (b == c) {
+                      jednakowe++;
+                   }
+                   return jednakowe;
+               }
+
+    /**
      * Główna fnukcja programu.
      * @param {float} a - Długość pierwszego boku.
      * @param {float} b - Długość drugiegio boku.
      * @param {float} c - Długość trzeciego boku.
      */
     public static void jakiTrojkat(float a, float b, float c){
-         if (a == b && b == c) {
+         int jednakoweDlugosci = ileJednakowych(a, b, c); // tutaj zliczamy ile jest jednakowych bokow
+                   if (jednakoweDlugosci > 1) {
+                       System.out.println("Trójkąt równoboczny"); 
+                   } else if (jednakoweDlugosci > 0) {
+                       System.out.println("Trójkąt równoramienny");
+                   }
+	 if (a == b && b == c) {
              System.out.println("Trójkąt równoboczny"); 
-         }
+         
+	 }
 	 if (a == b || b == c || a == c) {
-            System.out.println("Trójkąt równoramienny");
-                }
+	 System.out.println("Trójkąt równoramienny");
+	 }
+
          // TODO: tutaj trzeba bedzie dopisac inne przypadki
     }
     /** Wyświetla ekran pomocy */
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
-        System.out.println("Program do rozpoznawania rodzaju trójkąta");
+        System.out.println("Program do rozpoznawania rodzaju trójkąta!");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
     /** Glowna funkcja */
@@ -33,12 +58,13 @@ class Trojkaty {
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
+        
+	if (a < 0 || b < 0 || c < 0) {
+	System.out.println("Długości boków trójkąta muszą być nieujemne!");
+	System.exit(2);
+	}
 
-                if (a < 0 || b < 0 || c < 0) {
-                    System.out.println("Długości boków trójkąta muszą być nieujemne!");
-                    System.exit(2);
-                }
-
-                jakiTrojkat(a, b, c);
+	jakiTrojkat(a, b, c);
+        
     }
 }
