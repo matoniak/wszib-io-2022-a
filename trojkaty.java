70e8e3ebcb4b76393982f47d06c3d17dd298381d
Marcin Józef Matoniak <matoniak@artemis.wszib.edu.pl>
2022-06-18 15:53:46 +0200

    Weryfikacja wejścia: warunek istnienia trójkąta.

--- trojkaty.java
+++ trojkaty.java
@@ -4,13 +4,12 @@
 
 class Trojkaty {
     /**
-     * Główna fnukcja programu.
-     * @param {float} a - Długość pierwszego boku.
-     * @param {float} b - Długość drugiegio boku.
-     * @param {float} c - Długość trzeciego boku.
-     */
-<<<<<<< HEAD
-public static int ileJednakowych(float a, float b, float c){
+                * Funkcja zwraca ile par odcinków trójkąta ma jednakową długość.
+                * @param {float} a - Długość pierwszego boku.
+                * @param {float} b - Długość drugiego boku.
+                * @param {float} c - Długość trzeciego boku.
+                */
+               public static int ileJednakowych(float a, float b, float c){
                    int jednakowe = 0; // tutaj zliczamy ile jest jednakowych bokow
                    if (a == b) {
                       jednakowe++;
@@ -20,17 +19,22 @@ public static int ileJednakowych(float a, float b, float c){
                    }
                    return jednakowe;
                }
-=======
 
->>>>>>> a61f47d62058c9e08a28e7146b47e32f183e9d1a
-public static boolean czyIstniejeTrojkat(float a, float b, float c){
+    /**
+     * Główna fnukcja programu.
+     * @param {float} a - Długość pierwszego boku.
+     * @param {float} b - Długość drugiegio boku.
+     * @param {float} c - Długość trzeciego boku.
+     */
+
+    public static boolean czyIstniejeTrojkat(float a, float b, float c){
                   if (a + b <= c) return false;
                   if (a + c <= b) return false;
                   if (b + c <= a) return false;
                   return true;
                }
 
-public static boolean czyProstokatny(float a, float b, float c){
+    public static boolean czyProstokatny(float a, float b, float c){
                    if (a * a + b * b == c*c) return true;
                    if (b * b + c * c == a*a) return true;
                    if (a * a + c * c == b*b) return true;
@@ -39,35 +43,33 @@ public static boolean czyProstokatny(float a, float b, float c){
 
 
     public static void jakiTrojkat(float a, float b, float c){
-<<<<<<< HEAD
-int jednakoweDlugosci = ileJednakowych(a, b, c); // tutaj zliczamy ile jest jednakowych bokow
+         int jednakoweDlugosci = ileJednakowych(a, b, c); // tutaj zliczamy ile jest jednakowych bokow
                    if (jednakoweDlugosci > 1) {
                        System.out.println("Trójkąt równoboczny"); 
                    } else if (jednakoweDlugosci > 0) {
                        System.out.println("Trójkąt równoramienny");
                    }
-=======
-if (czyProstokatny(a, b, c)) {
-                    System.out.println("Trójkąt prostokątny");
-                  }
->>>>>>> a61f47d62058c9e08a28e7146b47e32f183e9d1a
-         if (a == b && b == c) {
+	 if (a == b && b == c) {
              System.out.println("Trójkąt równoboczny"); 
-         }
+         
+	 }
 	 if (a == b || b == c || a == c) {
-            System.out.println("Trójkąt równoramienny");
-                }
-         // TODO: tutaj trzeba bedzie dopisac inne przypadki
+	 System.out.println("Trójkąt równoramienny");
+	 }
+         if (czyProstokatny(a, b, c)) {
+         System.out.println("Trójkąt prostokątny");
+                  }
+      
     }
     /** Wyświetla ekran pomocy */
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
-        System.out.println("Program do rozpoznawania rodzaju trójkąta");
+        System.out.println("Program do rozpoznawania rodzaju trójkąta!");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
     /** Glowna funkcja */
     public static void main(String... args) {
- if (!czyIstniejeTrojkat(a, b, c)) {
+        if (!czyIstniejeTrojkat(a, b, c)) {
                     System.out.println("Z podanych długości boków nie da się zbudować trójkąta");
                     System.exit(3);
                    }
@@ -78,12 +80,13 @@ if (czyProstokatny(a, b, c)) {
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
+        
+	if (a < 0 || b < 0 || c < 0) {
+	System.out.println("Długości boków trójkąta muszą być nieujemne!");
+	System.exit(2);
+	}
 
-                if (a < 0 || b < 0 || c < 0) {
-                    System.out.println("Długości boków trójkąta muszą być nieujemne!");
-                    System.exit(2);
-                }
-
-                jakiTrojkat(a, b, c);
+	jakiTrojkat(a, b, c);
+        
     }
 }
